843bb2f6360e848126b279a1b0eb2a813f06a768
Ar B <cellmax@proton.me>
2025-01-19 14:54:37 +0100

    add

---
+++ src/main/java/org/acme/model/Actor.java
@@ -0,0 +1,33 @@
+package org.acme.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+
+
+/**
+ * Example JPA entity defined as a Panache Entity.
+ * An ID field of Long type is provided, if you want to define your own ID field extends <code>PanacheEntityBase</code> instead.
+ *
+ * This uses the active record pattern, you can also use the repository pattern instead:
+ * .
+ *
+ * Usage (more example on the documentation)
+ *
+ * {@code
+ *     public void doSomething() {
+ *         Movie entity1 = new Movie();
+ *         entity1.field = "field-1";
+ *         entity1.persist();
+ *
+ *         List<Movie> entities = Movie.listAll();
+ *     }
+ * }
+ */
+@Entity
+public class Actor extends PanacheEntity {
+    public String field;
+    public String name;
+    public String profile_path;
+    public String character;
+    public Long id;
+}
---
+++ src/main/java/org/acme/resources/MovieResource.java
@@ -0,0 +1,77 @@
+package org.acme;
+
+import java.net.URI;
+import java.util.List;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/movies")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class MovieResource {
+
+    @GET
+    public List<Movie> list() {
+        return Movie.listAll();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Movie get(Long id) {
+        return Movie.findById(id);
+    }
+
+    @POST
+    @Transactional
+    public Response create(Movie movie) {
+        movie.persist();
+        return Response.created(URI.create("/movies/" + movie.id)).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    public Movie update(Long id, Movie movie) {
+        Movie entity = Movie.findById(id);
+        if(entity == null) {
+            throw new NotFoundException();
+        }
+
+        // map all fields from the movie parameter to the existing entity
+        entity.name = movie.name;
+
+        return entity;
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public void delete(Long id) {
+        Movie entity = Movie.findById(id);
+        if(entity == null) {
+            throw new NotFoundException();
+        }
+        entity.delete();
+    }
+
+    @GET
+    @Path("/search/{name}")
+    public Movie search(String name) {
+        return Movie.findByName(name);
+    }
+
+    @GET
+    @Path("/count")
+    public Long count() {
+        return Movie.count();
+    }
+}
