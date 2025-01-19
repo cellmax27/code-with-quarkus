7300e88073b2b7117432f09d1104d8da2d70016f
Ar B <cellmax@proton.me>
2025-01-19 14:56:50 +0100

    add

---
+++ src/main/java/org/acme/services/GreetingService.java
@@ -0,0 +1,12 @@
+package org.acme.services;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+
+    public String greeting(String name) {
+        return "hello " + name;
+    }
+
+}
