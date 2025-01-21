7e218b76ccc35769e0c9d3863c7c38ed3744fd44
Ar B <cellmax@proton.me>
2025-01-21 22:42:52 +0100

    up attributes of movie enity [WIP]

--- src/main/java/org/acme/model/Movie.java
+++ src/main/java/org/acme/model/Movie.java
@@ -32,7 +32,38 @@ public class Movie extends PanacheEntity {
     public String name;
     public LocalDate birth;
 //    public Status status;
+    
+    // TODO maj API file
+    // angular attributes
+    public boolean adult; //: boolean;
+    public String backdrop_path; //: string;
+    public Long[] genre_ids; //: number[];
+    public String original_language; //: string;
+    public String original_title; //: string;
+    public String overview; //: string;
+    public Long popularity; //: number;
+    public String poster_path; //: string;
+    public LocalDate release_date;//: string;
+    public String title; //: string;
+    public boolean video; //: boolean;
+    public Long vote_average; //: number;
+    public Long vote_count; //: number;
+//    public Long revenue?; //: number;
+//    public String runtime?; //: string;
 
+    //public Long[Genre] genres?; //: any[];
+    
+    // csharp
+//    public string Director { get; set; }
+//    public List<string> Actors { get; set; }
+//    public double Rating { get; set; }
+//    public string Description { get; set; }
+//    public string Language { get; set; }
+//    public int Duration { get; set; } // Duration in minutes
+//    public string Country { get; set; }
+//    public bool IsAvailableOnNetflix { get; set; }
+
+    
     public static Movie findByName(String name){
         return find("name", name).firstResult();
     }
