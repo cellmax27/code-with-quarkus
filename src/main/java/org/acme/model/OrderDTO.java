5a8b22fa07c4fa0c5761a4fb38750ad36dfee6b9
Arnaud B <cellmax@proton.me>
2025-02-20 20:45:20 +0100

    clean

--- src/main/java/org/acme/model/OrderDTO.java
+++ src/main/java/org/acme/model/OrderDTO.java
@@ -5,24 +5,6 @@ import java.util.Date;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
 
-/**
- * Example JPA entity defined as a Panache Entity. An ID field of Long type is
- * provided, if you want to define your own ID field extends
- * <code>PanacheEntityBase</code> instead.
- *
- * This uses the active record pattern, you can also use the repository pattern
- * instead: .
- *
- * Usage (more example on the documentation)
- *
- * {@code
- *     public void doSomething() {
- *         Movie entity1 = new Movie();
- *         entity1.field = "field-1";
- *         entity1.persist();
- *
- *         List<Movie> entities = Movie.listAll(); } }
- */
 @Entity
 public class OrderDTO extends PanacheEntity {
 	public Long id;
